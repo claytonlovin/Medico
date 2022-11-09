e4a5a2931d55d5a565f53c74850115ab0802b22d
Clayton Silva <57988870+claytonlovin@users.noreply.github.com>
2022-11-09 01:41:41 -0300

    Execução da aplicação

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ app.include_router(Paciente.router)
 app.openapi = custom_openapi
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0')
